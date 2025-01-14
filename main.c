f8d930cf28308041770c5584d9a9ed861efaf213
Evan Tremblay <evan.tremblay@stu.ocsb.ca>
2025-01-14 01:01:00 -0500

    Update file: /home/mrcodester/student/main.c

--- main.c
+++ main.c
@@ -9,6 +9,9 @@ int main(int argc, char *argv[]){
 	} student;
 	student students[50];
 	student jerome={100.00f,"Jerome","Black","Jerome Black"};
+	student sofie={90.00f,"Sofie","White","Sofie White"};
 	students[0]=jerome;
+	students[1]=sofie;
 	printf("%s\n%s\n%s\n%.2f\n", students[0].firstname, students[0].lastname, students[0].displayname, students[0].grade);
+	printf("%s\n%s\n%s\n%.2f\n", students[1].firstname, students[1].lastname, students[1].displayname, students[1],grade)'
 }
